0eda3686cb7ee137a653ba296df979ac3378d033
Daniel P <daniel@brightstorm.de>
2013-02-08 22:45:51 +0100

    removed snakeyaml shit

--- src/de/brightstorm/Config.java
+++ src/de/brightstorm/Config.java
@@ -1,14 +1,10 @@
 package de.brightstorm;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 
-import org.apache.commons.io.FileUtils;
 import org.bukkit.plugin.Plugin;
-import org.yaml.snakeyaml.Yaml;
-
 import de.brightstorm.config.group;
 
 public class Config {
@@ -16,13 +12,11 @@ public class Config {
 		File configFile= new File(plugin.getDataFolder() + System.getProperty("file.separator") + "users.yml");
 		if(!configFile.exists()) {
 			plugin.getLogger().info("config.yml not found. Creating a new one...");
-			FileUtils.copyURLToFile(getClass().getResource("config.yml"), configFile);
+//			FileUtils.copyURLToFile(getClass().getResource("config.yml"), configFile);
 		}
-		config.load(new FileInputStream(configFile));
 		
 	}
 	
-	private Yaml config;
 	public boolean money;
 	public boolean autoUpdate;
 	public ArrayList<group> groups = new ArrayList<group>();
